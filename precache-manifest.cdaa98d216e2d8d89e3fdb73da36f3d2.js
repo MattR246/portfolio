467c56aa2e1cbe32c52aab60765a2220da080dae
MattR246 <49105348+MattR246@users.noreply.github.com>
2020-01-28 17:59:16 -0600

    Updates

---
+++ precache-manifest.cdaa98d216e2d8d89e3fdb73da36f3d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c120581faf5dc7d241a83c7515c9445",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "993f48b5f671918b9273",
+    "url": "/portfolio/static/css/main.263c0582.chunk.css"
+  },
+  {
+    "revision": "9a3c5175c443c3220df8",
+    "url": "/portfolio/static/js/2.95b5a3cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.95b5a3cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "993f48b5f671918b9273",
+    "url": "/portfolio/static/js/main.1e5cb731.chunk.js"
+  },
+  {
+    "revision": "2a1fa16ee482ba4b7704",
+    "url": "/portfolio/static/js/runtime-main.4188c15a.js"
+  },
+  {
+    "revision": "f1d1d619fcfc4682603d99725335a849",
+    "url": "/portfolio/static/media/profile.f1d1d619.png"
+  },
+  {
+    "revision": "0c860dfa4c12e0318b9a4e59fee7f810",
+    "url": "/portfolio/static/media/simplifica.0c860dfa.woff2"
+  },
+  {
+    "revision": "f112cdbf787fe7b1d47d7edd3a9fb278",
+    "url": "/portfolio/static/media/simplifica.f112cdbf.woff"
+  }
+]);
